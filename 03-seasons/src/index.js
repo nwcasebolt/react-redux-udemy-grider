133d19ad8679fa04ef33157a9b66fef8b2d7df4d
Nathan Casebolt <nwcasebolt@gmail.com>
2020-07-14 06:22:09 -0500

    Finished Section 5, State in React Components

--- 03-seasons/src/index.js
+++ 03-seasons/src/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+
+    // React does not require an explicit definition of constructor
+    constructor(props) {
+        super(props);
+
+        // this is the only time we do direct assignment to state
+        this.state = { lat: null, errorMessage: '' };
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            (position) => {
+
+                // setState extended from React.Component, ONLY way to change state
+                this.setState({ lat: position.coords.latitude })
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message })
+            }
         );
-        return <div>Latitude: </div>
+    }
+
+    // React requires a render method
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>;
     }
 }
 
