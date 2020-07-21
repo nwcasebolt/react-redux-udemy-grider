11e78d93eeac7c88d966627f7c8d8d8580a92ac2
Nathan Casebolt <nwcasebolt@gmail.com>
2020-07-21 07:51:23 -0500

    Section 8.88-90, API request using Axios

--- 04-pics/src/components/App.js
+++ 04-pics/src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        Axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID AQycZV7k9t5wSKEY6Xf8iZHxDcFZAOAHnPK9kfU-efQ'
+            }
+        });
     }
 
     render() {
